10e2d4a500e70d61e74a081c4953655d98b5cfc3
kshitij.khode <kshitij.khode@gmail.com>
2017-11-20 16:51:01 -0500

    [PYTHON] Add controller script to read images and feed to ML implementations ...
    [PYTHON] Add function to generate mace filter from images of a person

---
+++ controller.py
@@ -0,0 +1,19 @@
+import os, re
+
+import scipy as sp
+import numpy as np
+
+from scipy import misc
+
+imgsPerPerson = 4
+
+with open('LFW/peopleDevTrain.txt') as f:
+    dataDirs = [[l.split()[0], int(l.split()[1])] for l in f.readlines()[1:]]
+trainDirs = [i for i in dataDirs if i[1] >= imgsPerPerson]
+
+trainImgs = []
+for d in trainDirs:
+    pImgs = []
+    for i in range(imgsPerPerson):
+        pImgs.append(misc.imread('LFW/%s/%s_%04d.jpg'%(d[0], d[0], i+1)))
+    trainImgs.append(pImgs)
