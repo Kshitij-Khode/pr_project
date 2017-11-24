f043f459dc6f444bdcfd93055d7a66ba1143ed52
kshitij.khode <kshitij.khode@gmail.com>
2017-11-24 15:03:47 -0500

    [PYTHON][MISC] Added lfw dataset into the repo itself

--- controller.py
+++ controller.py
@@ -7,7 +7,7 @@ from scipy import misc
 
 imgsPerPerson = 4
 
-with open('LFW/peopleDevTrain.txt') as f:
+with open('lfw/peopleDevTrain.txt') as f:
     dataDirs = [[l.split()[0], int(l.split()[1])] for l in f.readlines()[1:]]
 trainDirs = [i for i in dataDirs if i[1] >= imgsPerPerson]
 
@@ -15,5 +15,5 @@ trainImgs = []
 for d in trainDirs:
     pImgs = []
     for i in range(imgsPerPerson):
-        pImgs.append(misc.imread('LFW/%s/%s_%04d.jpg'%(d[0], d[0], i+1)))
+        pImgs.append(misc.imread('lfw/%s/%s_%04d.jpg'%(d[0], d[0], i+1)))
     trainImgs.append(pImgs)
