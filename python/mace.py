8e8204e5d1209294bc4541761630b7b7bec3e07f
kshitij.khode <kshitij.khode@gmail.com>
2017-12-01 04:00:32 -0500

    [PYTHON] Add lfwDB.mat to ignore list and add python helpers

--- python/mace.py
+++ python/mace.py
@@ -1,20 +1,33 @@
 import numpy as np
 
 def trainMace(imgs):
+    imgDim   = imgs[0].shape
     fftImgs  = np.fft.fft2(imgs)
-    (c, s)   = fftImgs[0].shape[0]*fftImgs[0].shape[1]
-    fftImgs  = [i.reshape((c*s, 1)) for i in fftImgs]
+    fftImgs  = [i.flatten() for i in fftImgs]
     dFftImgs = [np.diag(np.square(np.absolute(i))) for i in fftImgs]
-    d        = np.zeros((c*s, 1))
+    d        = np.zeros(dFftImgs[0].shape)
+    fftImgs  = np.transpose(fftImgs);
 
     for dI in dFftImgs:
         d = np.add(d, dI)
 
-    u   = np.ones((len(imgs), 1))
+    u   = np.ones(len(imgs))
     mf0 = np.dot(np.linalg.inv(d), fftImgs)
     mf1 = np.dot(np.transpose(fftImgs), np.linalg.inv(d))
     mf1 = np.dot(mf1, fftImgs)
     maceFilt = np.dot(mf0, mf1)
-    maceFilt = np.dot(maceFilt, u).reshape(c, s)
+    maceFilt = np.dot(maceFilt, u).reshape()
 
     return maceFilt
+
+def applyMace(maceFilt, img):
+    return np.real(
+               np.fft.fftshift(
+                   np.fft.ifft2(
+                       np.multiply(maceFilt, np.conj(np.fft.fft2(img))))))
+
+def calculatePSR(maceResp):
+    (r, c)   = maceResp.shape
+    maceResp = maceResp.reshape((r*c, 1))
+
+    return (np.amax(maceResp) - np.mean(maceResp))/np.var(maceResp)
